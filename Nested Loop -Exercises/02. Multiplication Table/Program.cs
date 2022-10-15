96448e3f90c5c4cb0376aeb8149109c419225c1a
MartinSaviev <m.saviev@abv.bg>
2022-10-15 09:54:25 +0300

    Exercises

---
+++ Nested Loop -Exercises/02. Multiplication Table/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _02._Multiplication_Table
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int firstNumber = 1; firstNumber <= 10; firstNumber++)
+            {
+                for (int secoundNumber = 1; secoundNumber <=10; secoundNumber++)
+                {
+                    Console.WriteLine($"{firstNumber} * {secoundNumber} = {firstNumber*secoundNumber}");
+                }
+            }
+        }
+    }
+}
---
+++ Nested Loop -Exercises/04. Sum of Two Numbers/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _04._Sum_of_Two_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = int.Parse(Console.ReadLine());          
+            int end = int.Parse(Console.ReadLine());
+            int magicNum = int.Parse(Console.ReadLine());
+
+            int combinationCnt = 0;
+            bool comboFound = false;
+
+            for (int firstNum = start; firstNum <= end; firstNum++)
+            {
+                for (int secondNum = start; secondNum <= end; secondNum++)
+                {
+                    combinationCnt++;
+
+                    if (firstNum + secondNum == magicNum)
+                    {
+                        Console.WriteLine($"Combination N:{combinationCnt} ({firstNum} + {secondNum} = {magicNum})");
+                        comboFound = true;
+                        break;
+                    }
+                }
+
+                if (comboFound)
+                {
+                    break;
+                }
+            }
+
+            if (!comboFound)
+            {
+                Console.WriteLine($"{combinationCnt} combinations - neither equals {magicNum}");
+
+            }
+        }
+    }
+}
